d3c0fcb3bd13365fd2a4fa0a6f915ea8243b0734
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-12 22:31:43 +0530

    Time: 2 ms (99.51%), Space: 122.4 MB (90.31%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            if (num <= first) {
+                first = num;
+            }
+            else if (num <= second) {
+                second = num;
+            }
+            else {
+                // num > first and num > second
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
