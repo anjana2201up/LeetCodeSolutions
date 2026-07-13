673f5266193a4b20827e401e22bdde6360c16d1d
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-13 14:28:43 +0530

    Time: 21 ms (82%), Space: 59.7 MB (36.96%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class RecentCounter {
+
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
