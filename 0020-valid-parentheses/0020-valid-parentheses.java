d82ecdd9542460dc532e25d4baee2272414a2b89
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-14 15:16:41 +0530

    Time: 3 ms (87%), Space: 43.6 MB (15.86%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(')
+                stack.push(')');
+            else if (ch == '{')
+                stack.push('}');
+            else if (ch == '[')
+                stack.push(']');
+            else {
+
+                if (stack.isEmpty() || stack.pop() != ch)
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
