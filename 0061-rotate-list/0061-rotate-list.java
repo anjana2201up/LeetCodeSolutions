0fab797f7dc83fe91b3d2736d5681ba4755efdcb
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-14 09:27:39 +0530

    Time: 0 ms (100%), Space: 44.3 MB (44.93%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        // Find length and last node
+        ListNode tail = head;
+        int length = 1;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        // Reduce unnecessary rotations
+        k = k % length;
+
+        if (k == 0)
+            return head;
+
+        // Make circular linked list
+        tail.next = head;
+
+        // Find new tail
+        int steps = length - k;
+        ListNode newTail = head;
+
+        for (int i = 1; i < steps; i++) {
+            newTail = newTail.next;
+        }
+
+        // New head
+        ListNode newHead = newTail.next;
+
+        // Break the circle
+        newTail.next = null;
+
+        return newHead;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
