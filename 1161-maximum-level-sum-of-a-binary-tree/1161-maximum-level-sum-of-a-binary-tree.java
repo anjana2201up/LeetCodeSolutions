cd4a3078e7de3369234d35b3dee1bd14a779a45f
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-16 12:36:23 +0530

    Time: 9 ms (63.74%), Space: 49.2 MB (41.14%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int maxLevelSum(TreeNode root) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int level = 1;
+        int ans = 1;
+        int maxSum = Integer.MIN_VALUE;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            int sum = 0;
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+                sum += node.val;
+
+                if (node.left != null)
+                    queue.offer(node.left);
+
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                ans = level;
+            }
+
+            level++;
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
