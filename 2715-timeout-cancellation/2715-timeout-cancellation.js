b266ffe99af015e633f59141c99147ed35f1536a
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-17 22:03:05 +0530

    Time: 63 ms (21.5%), Space: 53.3 MB (93.66%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+
+    const timer = setTimeout(() => {
+        fn(...args);
+    }, t);
+
+    return function cancelFn() {
+        clearTimeout(timer);
+    };
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
