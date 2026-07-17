db9d6418865f010a39c8badf046d00d5860bc9f4
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-17 09:20:31 +0530

    Time: 5 ms (55.58%), Space: 46.5 MB (63.82%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1 || s.length() <= numRows)
+            return s;
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+
+            rows[currentRow].append(c);
+
+            if (currentRow == 0 || currentRow == numRows - 1)
+                goingDown = !goingDown;
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (StringBuilder row : rows)
+            ans.append(row);
+
+        return ans.toString();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
