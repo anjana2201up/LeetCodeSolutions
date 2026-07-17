66786959e8bb824c5c96695b369ec127b8364f04
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-17 21:42:14 +0530

    Time: 54 ms (13.35%), Space: 55.8 MB (11.23%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val = init;
+
+    for (let i = 0; i < nums.length; i++) {
+        val = fn(val, nums[i]);
+    }
+
+    return val;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
