64bdededf984c47231008600e33e92b56d209214
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-17 10:41:19 +0530

    Time: 34 ms (27.17%), Space: 155.3 MB (31.79%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+
+        Deque<Integer> dq = new LinkedList<>();
+
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove indices outside the current window
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            // Remove smaller elements from the back
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            // Add current index
+            dq.offerLast(i);
+
+            // Record the maximum once the first full window is formed
+            if (i >= k - 1) {
+                ans[index++] = nums[dq.peekFirst()];
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
