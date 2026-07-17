df68764f4d73dbd64eb5a0226e3708dd5a03d517
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-17 23:30:06 +0530

    Time: 556 ms (9.54%), Space: 0B (100%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
