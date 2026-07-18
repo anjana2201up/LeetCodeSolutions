da9235aafd40a58081ee154bcc51eadc59f86036
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-18 21:43:48 +0530

    Time: 246 ms (99.27%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(N)

---
+++ 1484-second-highest-salary/1484-second-highest-salary.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
