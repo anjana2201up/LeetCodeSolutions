fa2933be052a7afc7f8be7d239d84be9f0a01370
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-18 15:41:14 +0530

    Time: 739 ms (26%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 1527-fix-names-in-a-table/1527-fix-names-in-a-table.sql
@@ -0,0 +1,12 @@
+SELECT
+    user_id,
+    CONCAT(
+        UPPER(LEFT(name, 1)),
+        LOWER(SUBSTRING(name, 2))
+    ) AS name
+FROM Users
+ORDER BY user_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
