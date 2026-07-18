313a3bf2a9dffd97332330e4c2e27de3180bda95
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-18 15:43:18 +0530

    Time: 465 ms (20.72%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(1)

---
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT *
+FROM Patients
+WHERE conditions LIKE 'DIAB1%'
+   OR conditions LIKE '% DIAB1%';
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
