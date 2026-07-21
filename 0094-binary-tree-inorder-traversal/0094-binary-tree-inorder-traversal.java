e3ca7fb2793ebac25d0f36ebc7e9cc18fec6d68d
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-21 18:08:16 +0530

    Time: 0 ms (100%), Space: 43 MB (86.07%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
