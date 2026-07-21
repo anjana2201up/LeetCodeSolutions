70ba2b0d28261e0337ddacb85e7d170311817e7e
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-21 18:10:12 +0530

    Time: 0 ms (100%), Space: 43.6 MB (53.12%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isMirror(left.left, right.right)
+                && isMirror(left.right, right.left);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
