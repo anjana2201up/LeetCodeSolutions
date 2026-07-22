0de5b91c8e7741a70cbb7e9eae1e53c967a4447d
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-22 14:43:29 +0530

    Time: 0 ms (100%), Space: 42.7 MB (82.34%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        // Swap left and right children
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Recursively invert subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
