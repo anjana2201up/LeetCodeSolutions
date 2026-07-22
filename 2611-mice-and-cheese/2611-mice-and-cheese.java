70c95eb7f91a42d8c020e68e34c7fc22064a5b05
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-22 15:35:09 +0530

    Time: 64 ms (23.14%), Space: 77.1 MB (86.36%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 2611-mice-and-cheese/2611-mice-and-cheese.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int miceAndCheese(int[] reward1, int[] reward2, int k) {
+
+        int n = reward1.length;
+        Integer[] diff = new Integer[n];
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+            total += reward2[i];
+            diff[i] = reward1[i] - reward2[i];
+        }
+
+        Arrays.sort(diff, Collections.reverseOrder());
+
+        for (int i = 0; i < k; i++) {
+            total += diff[i];
+        }
+        return total;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
