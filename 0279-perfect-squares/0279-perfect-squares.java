d12920832821dc96150edd6d4978591b659a2cff
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-22 14:58:50 +0530

    Time: 36 ms (46.25%), Space: 44.5 MB (65.69%) - LeetHub - Time: O(n*sqrt(n)), Space: O(n)

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
