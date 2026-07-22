75bd6df672178d2e70f7576eae57c4b6f5c270a4
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-22 10:49:56 +0530

    Time: 4 ms (90.86%), Space: 83 MB (47.27%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+        // Phase 1: Find the meeting point
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find the entrance to the cycle
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
