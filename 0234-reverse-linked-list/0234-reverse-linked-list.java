6dbf17cb842fc23e43584e90c68a296f0d664692
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-23 23:41:02 +0530

    Time: 0 ms (100%), Space: 44.5 MB (13.88%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0234-reverse-linked-list/0234-reverse-linked-list.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
