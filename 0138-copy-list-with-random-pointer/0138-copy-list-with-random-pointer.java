cda460df2248fe5e3fe6d6b832e3f461483d7c3e
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-23 22:47:16 +0530

    Time: 0 ms (100%), Space: 46.7 MB (32.22%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        HashMap<Node, Node> map = new HashMap<>();
+
+        // Step 1: Copy each node
+        Node curr = head;
+        while (curr != null) {
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+
+        // Step 2: Set next and random pointers
+        curr = head;
+        while (curr != null) {
+            Node copy = map.get(curr);
+            copy.next = map.get(curr.next);
+            copy.random = map.get(curr.random);
+            curr = curr.next;
+        }
+
+        return map.get(head);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
