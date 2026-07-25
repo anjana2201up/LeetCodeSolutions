fad1a5c4917e7cb0a91dec9e433f14bec8b70cd3
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-25 23:25:15 +0530

    Time: 0 ms (100%), Space: 42.8 MB (93.99%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0002-time-needed-to-buy-tickets/0002-time-needed-to-buy-tickets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0;
+
+        for(int i = 0; i < tickets.length; i++) {
+            if(i <= k) {
+                time += Math.min(tickets[i], tickets[k]);
+            } else {
+                time += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return time;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
