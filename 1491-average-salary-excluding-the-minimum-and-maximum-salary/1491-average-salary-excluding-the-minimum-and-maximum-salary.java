b0f753af6808a4b5cbb438be5d8d9b4798cfd97b
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-25 12:14:43 +0530

    Time: 0 ms (100%), Space: 45.7 MB (40.56%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double average(int[] salary) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int s : salary) {
+            min = Math.min(min, s);
+            max = Math.max(max, s);
+            sum += s;
+        }
+        return (double)(sum - min - max) / (salary.length - 2);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
