5fe6da557c3130682b7dbe7883e5ee14ad16c34e
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-25 21:46:33 +0530

    Time: 0 ms (100%), Space: 53 MB (39.07%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0852-single-element-in-a-sorted-array/0852-single-element-in-a-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution{
+    public int singleNonDuplicate(int[] nums){
+        int l=0,r=nums.length-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(m%2==1)m--;
+            if(nums[m]==nums[m+1])l=m+2;
+            else r=m;
+        }
+        return nums[l];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
