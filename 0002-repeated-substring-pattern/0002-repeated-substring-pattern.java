14bf12688f924a85c69629943c7fd2319cabd959
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-25 23:37:33 +0530

    Time: 74 ms (56.64%), Space: 46.9 MB (54.19%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0002-repeated-substring-pattern/0002-repeated-substring-pattern.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
