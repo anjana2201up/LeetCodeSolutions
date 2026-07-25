33d441ac69a1651811a64f673269763f24aeff87
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-25 21:52:23 +0530

    Time: 25 ms (99.71%), Space: 45.8 MB (22.09%) - LeetHub - Time: O(n^2), Space: O(1)

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -2,24 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-
         Arrays.sort(potions);
-
         int n = spells.length;
         int m = potions.length;
-
         int[] ans = new int[n];
-
         for (int i = 0; i < n; i++) {
-
             int left = 0;
             int right = m - 1;
             int index = m;
-
             while (left <= right) {
-
                 int mid = left + (right - left) / 2;
-
                 if ((long) spells[i] * potions[mid] >= success) {
                     index = mid;
                     right = mid - 1;
