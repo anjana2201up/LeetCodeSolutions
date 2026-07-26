b0ff9e4169dbbd7acaeb11c9e894c28cb91d187c
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-26 21:49:44 +0530

    Time: 43 ms (75.35%), Space: 64.9 MB (10.63%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums,0,nums.length-1);
+    }
+
+    public int mergeSort(int[] nums,int left,int right){
+        if(left>=right){
+            return 0;
+        }
+        int mid=left+(right-left)/2;
+        int count=mergeSort(nums,left,mid)+mergeSort(nums,mid+1,right);
+
+        int j=mid+1;
+        for(int i=left;i<=mid;i++){
+            while(j<=right&&(long)nums[i]>2L*nums[j]){
+                j++;
+            }
+            count+=j-(mid+1);
+        }
+
+        merge(nums,left,mid,right);
+        return count;
+    }
+
+    public void merge(int[] nums,int left,int mid,int right){
+        int[] temp=new int[right-left+1];
+        int i=left,j=mid+1,k=0;
+
+        while(i<=mid&&j<=right){
+            if(nums[i]<=nums[j]){
+                temp[k++]=nums[i++];
+            }else{
+                temp[k++]=nums[j++];
+            }
+        }
+
+        while(i<=mid){
+            temp[k++]=nums[i++];
+        }
+
+        while(j<=right){
+            temp[k++]=nums[j++];
+        }
+
+        for(int x=0;x<temp.length;x++){
+            nums[left+x]=temp[x];
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
