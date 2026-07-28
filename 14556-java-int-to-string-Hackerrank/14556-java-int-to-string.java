9c4fd104580f5693bbe869f57a6a90cd226a91fc
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-07-28 15:49:58 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 14556-java-int-to-string-Hackerrank/14556-java-int-to-string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=String.valueOf(n);
+        System.out.println("Good job");
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
