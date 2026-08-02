e4d858db9cb7db9994c6b35bd697bbb89d20e942
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-08-02 21:42:29 +0530

    Time: 83 ms (16.38%), Space: 86.2 MB (47.79%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0003-create-binary-tree-from-descriptions/0003-create-binary-tree-from-descriptions.java
@@ -0,0 +1,35 @@
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> map = new HashMap<>();
+        Set<Integer> children = new HashSet<>();
+
+        for (int[] d : descriptions) {
+            int parent = d[0];
+            int child = d[1];
+            int isLeft = d[2];
+
+            map.putIfAbsent(parent, new TreeNode(parent));
+            map.putIfAbsent(child, new TreeNode(child));
+
+            if (isLeft == 1) {
+                map.get(parent).left = map.get(child);
+            } else {
+                map.get(parent).right = map.get(child);
+            }
+
+            children.add(child);
+        }
+
+        for (int[] d : descriptions) {
+            if (!children.contains(d[0])) {
+                return map.get(d[0]);
+            }
+        }
+
+        return null;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
