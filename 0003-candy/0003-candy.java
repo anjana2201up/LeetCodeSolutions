1925fe70ff8ab72c6dad96cb25cbd942cb20501c
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-08-02 21:57:34 +0530

    Time: 3 ms (86.41%), Space: 47.2 MB (96.3%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0003-candy/0003-candy.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int ans = 0;
+        for (int candy : candies) {
+            ans += candy;
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
