49cee729b9cd4cc13eb217956854da192c531913
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-08-03 09:49:12 +0530

    Time: 28 ms (41.37%), Space: 100.7 MB (39.55%) - LeetHub - Time: O(1), Space: O(N)

---
+++ 0001-insert-delete-getrandom-o1/0001-insert-delete-getrandom-o1.java
@@ -0,0 +1,41 @@
+class RandomizedSet {
+    private List<Integer> list;
+    private Map<Integer, Integer> map;
+    private Random random;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+
+        int index = map.get(val);
+        int last = list.get(list.size() - 1);
+
+        list.set(index, last);
+        map.put(last, index);
+
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
