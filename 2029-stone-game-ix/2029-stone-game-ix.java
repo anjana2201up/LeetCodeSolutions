33c7b6a6a3ccb9714a1a821b52edef21827e203d
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-08-16 20:32:23 +0530

    Time: 4 ms (75.68%), Space: 114.5 MB (97.97%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] cnt = new int[3];
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] > 0 && cnt[2] > 0;
+        }
+
+        return Math.abs(cnt[1] - cnt[2]) > 2;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
