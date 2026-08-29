20531250f7045cf88a446d3c51b05cdb0b5061bc
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-08-29 22:47:28 +0530

    Time: 72 ms (89.39%), Space: 155.5 MB (68.94%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int start = 0;
+
+        while (start < n) {
+            int end = start;
+
+            while (end + 1 < n &&
+                   (long) arr[end + 1][0] - arr[end][0] <= limit) {
+                end++;
+            }
+
+            int[] values = new int[end - start + 1];
+            int[] indices = new int[end - start + 1];
+
+            for (int i = start; i <= end; i++) {
+                values[i - start] = arr[i][0];
+                indices[i - start] = arr[i][1];
+            }
+
+            Arrays.sort(indices);
+
+            for (int i = 0; i < values.length; i++) {
+                nums[indices[i]] = values[i];
+            }
+
+            start = end + 1;
+        }
+
+        return nums;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
