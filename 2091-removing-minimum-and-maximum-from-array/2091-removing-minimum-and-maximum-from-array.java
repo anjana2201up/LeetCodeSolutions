bb58354b9f4294ec1e8cdbec58b12551ba0178f7
ANJANA KUMARI <sharmaanjana5965@gmail.com>
2026-08-30 23:17:17 +0530

    Time: 3 ms (61.82%), Space: 85.8 MB (96.55%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minimumDeletions(int[] nums) {
+        int n = nums.length;
+
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < nums[minIndex]) {
+                minIndex = i;
+            }
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        int left = Math.min(minIndex, maxIndex);
+        int right = Math.max(minIndex, maxIndex);
+
+        int option1 = right + 1;
+        int option2 = n - left;
+        int option3 = left + 1 + n - right;
+
+        return Math.min(option1, Math.min(option2, option3));
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
